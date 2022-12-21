0f3682b10f2af138b04a2d406e8c219fd94fa882
Astra Sword <83428598+astrasword@users.noreply.github.com>
2022-12-21 12:30:57 +0000

    tests: make a simple test suite via _quarto.yml
    Unfortunately, there's no simple way to run tests to see if the documents in
    this template are correctly rendered. To remedy this, I've created a set of
    testing documents that can all be rendered together as a quarto project. I've
    also added some bash scripts that will move the intermediate tex and bib files
    generated by quarto into corresponding folders in test-results (TODO: replace
    the sh files with something that's more compatible across platforms). It's my
    hope that by doing this we can write simple tests for the LaTeX itself in the
    future.

---
+++ scripts/move-intermediate-files-to-test-results.sh
@@ -0,0 +1,4 @@
+mkdir test-results/tex
+mv ./tests/*.tex test-results/tex
+mkdir test-results/bib
+mv ./tests/*.bib test-results/bib
---
+++ scripts/rename-test-folder-to-pdf.sh
@@ -0,0 +1,3 @@
+mkdir test-results/pdf
+mv test-results/tests/* test-results/pdf
+rm -d test-results/tests
